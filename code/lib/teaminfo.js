b62fe3aeb2a474bafa3f4658d50c0b9ddbede48d
eunbeann <ds5bean23@gmail.com>
2024-02-01 16:17:48 +0900

    first commit

---
+++ code/lib/teaminfo.js
@@ -0,0 +1,82 @@
+module.export = [
+  {
+    clubName : 'wyverns',
+    clubEnglishName: 'SK Wyverns',
+    description : '엄청 멋진 팀',
+    teamBase: '인천',
+    homeGround: '인천',
+    images: [{url:'/images/SK_Wyverns.svg.png'}]
+  },
+  {
+    clubName : 'bears',
+    clubEnglishName: 'Doosan Bears',
+    description : '엄청 멋진 팀',
+    teamBase: '서울',
+    homeGround: '서울',
+    images: [{url:'/images/SK_Wyverns.svg.png'}]
+  },
+  {
+    clubName : 'eaglse',
+    clubEnglishName: 'Hanhwa Eagles',
+    description : '엄청 멋진 팀',
+    teamBase: '대전',
+    homeGround: '대전',
+    images: [{url:'/images/SK_Wyverns.svg.png'}]
+  },
+  {
+    clubName : 'heros',
+    clubEnglishName: 'Nexen Heros',
+    description : '엄청 멋진 팀',
+    teamBase: '서울',
+    homeGround: '서울',
+    images: [{url:'/images/SK_Wyverns.svg.png'}]
+  },
+  {
+    clubName : 'tigers',
+    clubEnglishName: 'Kia Tigers',
+    description : '엄청 멋진 팀',
+    teamBase: '광주',
+    homeGround: '광주',
+    images: [{url:'/images/SK_Wyverns.svg.png'}]
+  },
+  {
+    clubName : 'lions',
+    clubEnglishName: 'Samaung Lions',
+    description : '엄청 멋진 팀',
+    teamBase: '대구',
+    homeGround: '대구',
+    images: [{url:'/images/SK_Wyverns.svg.png'}]
+  },
+  {
+    clubName : 'giants',
+    clubEnglishName: 'Lotte Giants',
+    description : '엄청 멋진 팀',
+    teamBase: '어디',
+    homeGround: '어디',
+    images: [{url:'/images/SK_Wyverns.svg.png'}]
+  },
+  {
+    clubName : 'twins',
+    clubEnglishName: 'LG Twins',
+    description : '엄청 멋진 팀',
+    teamBase: '어디',
+    homeGround: '어디',
+    images: [{url:'/images/SK_Wyverns.svg.png'}]
+  },
+  {
+    clubName : 'wiz',
+    clubEnglishName: 'KT wiz',
+    description : '엄청 멋진 팀',
+    teamBase: '수원',
+    homeGround: '수원',
+    images: [{url:'/images/SK_Wyverns.svg.png'}]
+  },
+  {
+    clubName : 'dynos',
+    clubEnglishName: 'NC dynos',
+    description : '엄청 멋진 팀',
+    teamBase: '창원',
+    homeGround: '창원',
+    images: [{url:'/images/SK_Wyverns.svg.png'}]
+  },
+]
